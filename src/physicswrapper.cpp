c6918cc920b5e7657014d42fede31afcada5b12c
FHorschig <f.horschig@googlemail.com>
2013-11-06 20:16:19 +0100

    Linking the libs does work now properly.

--- src/physicswrapper.cpp
+++ src/physicswrapper.cpp
@@ -1,5 +1,9 @@
 #include "physicswrapper.h"
 
+#pragma comment(lib, "PhysX3_x64.lib")
+#pragma comment(lib, "PhysX3Common_x64.lib")
+#pragma comment(lib, "PhysX3Extensions.lib")
+
 PhysicsWrapper::PhysicsWrapper(){
 	initialize();
 }
@@ -9,9 +13,27 @@ PhysicsWrapper::~PhysicsWrapper(){
 }
 
 void PhysicsWrapper::initialize(){
+	static physx::PxDefaultErrorCallback gDefaultErrorCallback;
+	static physx::PxDefaultAllocator gDefaultAllocatorCallback;
 
+	m_foundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
+	if (!m_foundation)
+		fatalError("PxCreateFoundation failed!");
+	m_physics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_foundation, physx::PxTolerancesScale());
+	if (!m_physics)
+		fatalError("PxCreatePhysics failed!");
 }
 
 void PhysicsWrapper::shutdown(){
+	m_foundation->release();
+	m_physics->release();
+}
 
+void PhysicsWrapper::fatalError(string error_message){
+	std::cerr << "PhysX Error occured:" << std::endl;
+	std::cerr << error_message << std::endl;
+	std::cerr << "Press Enter to close the Application" << std::endl;
+	string temp;
+	std::getline(std::cin, temp);
+	exit(1);
 }
--- src/physicswrapper.h
+++ src/physicswrapper.h
@@ -1,6 +1,14 @@
 #pragma once
 
+// Standard Libs
+#include <string>
+#include <iostream>
+
+using std::string;
+
+// NVIDIA PhysX Libs
 #include "PxPhysicsAPI.h"
+
 
 #define DISALLOW_COPY_AND_ASSIGN(TypeName)	\
 	TypeName(const TypeName&);				\
@@ -13,11 +21,13 @@ public:
 	~PhysicsWrapper();
 
 
-
 protected:
 	void initialize();
 	void shutdown();
+	void fatalError(string error_message);
 
+	physx::PxFoundation*	m_foundation = nullptr;
+	physx::PxPhysics*		m_physics = nullptr;
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(PhysicsWrapper);
