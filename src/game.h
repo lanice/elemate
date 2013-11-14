079990d6ef9de95461c69b8694d2e438b9d1d0e1
FHorschig <f.horschig@googlemail.com>
2013-11-14 12:11:52 +0100

    something moves!!! (It's ugly and dirty)

--- src/game.h
+++ src/game.h
@@ -11,8 +11,11 @@
 class PhysicsWrapper;
 class CyclicTime;
 namespace std {			class thread; }
-namespace osg {			class Geode;  }
+namespace osg {			class Group;  
+						class MatrixTransform;
+}
 namespace osgViewer {	class Viewer; }
+namespace physx{ class PxRigidDynamic; }
 
 /** The Game Class that invokes a game loop and initializes PhysX.
  *	To receive the initialized physics, call getPhysicsWrapper(). See for its usage the documentation of PhysicsWrapper class.
@@ -21,28 +24,28 @@ namespace osgViewer {	class Viewer; }
 class Game{
 public:
 
+	typedef std::pair<osg::MatrixTransform*, physx::PxRigidDynamic*> DrawableAndPhysXObject;
+
 	/** Explicit Constructor because Copying and Assignments are disabled. 	*/
 	explicit Game();	
-	
+	explicit Game(osgViewer::Viewer* viewer);
+
 	~Game();
 
-	/** 
-	  * Starts the Game Loop in a new thread until interrupt() is called. 	
-	  * @param spawn_new_thread If set true, the game loop is executed in a new thread and start() is not a blocking call.
-	  */
-	void start(bool spawn_new_thread);
+	/** Starts the Game Loop until end() is called.  */
+	void start();
 
 	/** True if the game loop is running. */
 	bool isRunning() const;
 
-	/** Calls interrupt() and waits for finishing ending the loop. */
+	/** Ending the loop. */
 	void end();
 
 	/** Returns properly initialized PhysicsWrapper. */
 	std::shared_ptr<PhysicsWrapper> physicsWrapper() const;
 
 protected:
-	void initialize();
+	void initialize(osgViewer::Viewer* viewer);
 
 	/** The Game's loop containing drawing and triggering physics is placed right here. */
 	void loop();
@@ -51,11 +54,12 @@ protected:
 	void setOsgCamera();
 
 	std::shared_ptr<PhysicsWrapper>	m_physics_wrapper;
-	std::thread*					m_thread;
 	osgViewer::Viewer*				m_viewer;
-	osg::ref_ptr<osg::Geode>		m_root;
+	bool							m_interrupted;
+	osg::ref_ptr<osg::Group>		m_root;
 	CyclicTime*						m_cyclic_time;
-
+	
+	DrawableAndPhysXObject			m_sphere;
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(Game);
