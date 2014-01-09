101313abe7587b7a1c8298300800b1d579b2cd7e
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2014-01-09 11:06:11 +0100

    add ifndef LINUX define to main.cpp to prevent crashing unter bumblebee/optirun

--- src/main.cpp
+++ src/main.cpp
@@ -72,9 +72,12 @@ int main()
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, MajorVersionRequire);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MinorVersionRequire);
+
+#ifndef LINUX // Running with bumblebee causes the following commands to crash.
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     if (MajorVersionRequire >= 3 && MinorVersionRequire >= 2)
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
     GLFWwindow * window = glfwCreateWindow(initialWidth, initialHeight, "Elemate", NULL, NULL);
     if (!window)
