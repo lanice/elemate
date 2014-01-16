24a5800bc90a9d3ac0e7a22cda4d5ed063c22169
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2014-01-16 14:27:32 +0100

    clean up and add const correctness. #76

--- src/luawrapper.cpp
+++ src/luawrapper.cpp
@@ -52,12 +52,12 @@ void LuaWrapper::reloadScripts()
     }
 }
 
-void LuaWrapper::pushFunc(const std::string & func)
+void LuaWrapper::pushFunc(const std::string & func) const
 {
     lua_getglobal(m_state, func.c_str());
 }
 
-void LuaWrapper::callFunc(const int & numArgs, const int & numRet)
+void LuaWrapper::callFunc(const int numArgs, const int numRet)
 {
     m_err = lua_pcall(m_state, numArgs, numRet, 0);
     luaError();
@@ -68,123 +68,83 @@ void LuaWrapper::popStack(lua_State * state, const int index)
     lua_pop(state, index);
 }
 
-void LuaWrapper::push()
+void LuaWrapper::push() const
 {
 }
 
-void LuaWrapper::push(const std::string & value)
+void LuaWrapper::push(const std::string & value) const
 {
     lua_pushstring(m_state, value.c_str());
 }
 
-void LuaWrapper::push(const char * value)
+void LuaWrapper::push(const char * value) const
 {
     lua_pushstring(m_state, value);
 }
 
-void LuaWrapper::push(const int value)
+void LuaWrapper::push(const int value) const
 {
     lua_pushinteger(m_state, value);
 }
 
-void LuaWrapper::push(const double value)
+void LuaWrapper::push(const double value) const
 {
     lua_pushnumber(m_state, value);
 }
 
-void LuaWrapper::push(const unsigned long value)
+void LuaWrapper::push(const unsigned long value) const
 {
     lua_pushunsigned(m_state, value);
 }
 
-void LuaWrapper::push(const bool value)
+void LuaWrapper::push(const bool value) const
 {
     lua_pushboolean(m_state, value);
-}
-
-void LuaWrapper::fetchResult(std::string & result)
-{
-    if (!lua_isstring(m_state, -1))
-    {
-        glow::critical("LuaWrapper: Return value not a string.");
-        return;
-    }
-    result = lua_tostring(m_state, -1);
-    lua_pop(m_state, 1);
-}
-
-void LuaWrapper::fetchResult(int & result)
-{
-    if (!lua_isnumber(m_state, -1))
-    {
-        glow::critical("LuaWrapper: Return value not a number.");
-        return;
-    }
-    assert( static_cast<int>(lua_tointeger(m_state, -1)) == lua_tointeger(m_state, -1));
-    result = static_cast<int>(lua_tointeger(m_state, -1));
-    lua_pop(m_state, 1);
-}
-
-void LuaWrapper::fetchResult(double & result)
-{
-    if (!lua_isnumber(m_state, -1))
-    {
-        glow::critical("LuaWrapper: Return value not a number.");
-        return;
-    }
-    result = lua_tonumber(m_state, -1);
-    lua_pop(m_state, 1);
-}
-
-void LuaWrapper::fetchResult(unsigned long & result)
-{
-    if (!lua_isnumber(m_state, -1))
-    {
-        glow::critical("LuaWrapper: Return value not a number.");
-        return;
-    }
-    result = lua_tounsigned(m_state, -1);
-    lua_pop(m_state, 1);
-}
-
-void LuaWrapper::fetchResult(bool & result)
-{
-    if (!lua_isboolean(m_state, -1))
-    {
-        glow::critical("LuaWrapper: Return value not a boolean.");
-        return;
-    }
-    result = lua_toboolean(m_state, -1) != 0;
-    lua_pop(m_state, 1);
 }
 
 
 template<>
 std::string LuaWrapper::fetch<std::string>(const int index) const
 {
+    if (!lua_isstring(m_state, index))
+        glow::critical("LuaWrapper: Return value not a string.");
+
     return lua_tostring(m_state, index);
 }
 
 template<>
 int LuaWrapper::fetch<int>(const int index) const
 {
-    return lua_tointeger(m_state, index);
+    if (!lua_isnumber(m_state, index))
+        glow::critical("LuaWrapper: Return value not a number.");
+
+    assert( static_cast<int>(lua_tointeger(m_state, index)) == lua_tointeger(m_state, index));
+    return static_cast<int>(lua_tointeger(m_state, index));
 }
 
 template<>
 double LuaWrapper::fetch<double>(const int index) const
 {
+    if (!lua_isnumber(m_state, index))
+        glow::critical("LuaWrapper: Return value not a number.");
+
     return lua_tonumber(m_state, index);
 }
 
 template<>
 unsigned long LuaWrapper::fetch<unsigned long>(const int index) const
 {
+    if (!lua_isnumber(m_state, index))
+        glow::critical("LuaWrapper: Return value not a number.");
+
     return lua_tounsigned(m_state, index);
 }
 
 template<>
 bool LuaWrapper::fetch<bool>(const int index) const
 {
+    if (!lua_isboolean(m_state, index))
+        glow::critical("LuaWrapper: Return value not a boolean.");
+
     return lua_toboolean(m_state, index);
 }
--- src/luawrapper.h
+++ src/luawrapper.h
@@ -17,81 +17,54 @@ public:
     void removeScript(const std::string & script);
     void reloadScripts();
 
-    // template<typename... TArgs>
-    // void callFunc(const std::string & func, const int & resultCount, const TArgs & ... args)
-    // {
-    //     pushFunc(func);
-    //     push(args...);
-    //     callFunction(sizeof...(args), resultCount);
-    // };
-
-    // void fetchResults(){};
-    // template<typename T, typename... TResults>
-    // void fetchResults(T & result, TResults & ... results)
-    // {
-    //     fetchResults(results...);
-    //     fetchResult(result);
-    // };
-
 
 protected:
     void luaError();
-
-    void pushFunc(const std::string & func);
-    void callFunc(const int & numArgs, const int & numRet);
+    void pushFunc(const std::string & func) const;
+    void callFunc(const int numArgs, const int numRet);
 
     static void popStack(lua_State * state, const int index);
 
     template<typename T, typename... Ts>
-    void push(const T value, const Ts... values)
+    void push(const T value, const Ts... values) const
     {
         push(value);
         push(values...);
     };
 
-    void push();
-    void push(const char * value);
-    void push(const std::string & value);
-    void push(const int value);
-    void push(const double value);
-    void push(const unsigned long value);
-    void push(const bool value);
-
-
-    void fetchResult(std::string & result);
-    void fetchResult(int & result);
-    void fetchResult(double & result);
-    void fetchResult(unsigned long & result);
-    void fetchResult(bool & result);
+    void push() const;
+    void push(const std::string & value) const;
+    void push(const char * value) const;
+    void push(const int value) const;
+    void push(const double value) const;
+    void push(const unsigned long value) const;
+    void push(const bool value) const;
 
     template<typename T> T fetch(const int index) const;
 
 
-    // General type trait struct
     template <size_t, typename... Ts>
     struct _pop
     {
         typedef std::tuple<Ts...> type;
 
-        // base case: creates a tuple containing one element
         template <typename T>
-        static std::tuple<T> worker(const LuaWrapper &l, const int index)
+        static std::tuple<T> worker(const LuaWrapper &instance, const int index)
         {
-            return std::make_tuple(l.fetch<T>(index));
+            return std::make_tuple(instance.fetch<T>(index));
         }
 
-        // inductive case
         template <typename T1, typename T2, typename... Rest>
-        static std::tuple<T1, T2, Rest...> worker(const LuaWrapper &l, const int index)
+        static std::tuple<T1, T2, Rest...> worker(const LuaWrapper &instance, const int index)
         {
-            std::tuple<T1> head = std::make_tuple(l.fetch<T1>(index));
-            return std::tuple_cat(head, worker<T2, Rest...>(l, index + 1));
+            std::tuple<T1> head = std::make_tuple(instance.fetch<T1>(index));
+            return std::tuple_cat(head, worker<T2, Rest...>(instance, index + 1));
         }
 
-        static type apply(LuaWrapper &l)
+        static type apply(LuaWrapper &instance)
         {
-            auto ret = worker<Ts...>(l, 1);
-            LuaWrapper::popStack(l.m_state, sizeof...(Ts));
+            auto ret = worker<Ts...>(instance, 1);
+            LuaWrapper::popStack(instance.m_state, sizeof...(Ts));
             return ret;
         }
     };
@@ -100,50 +73,42 @@ protected:
     struct _pop<0, Ts...>
     {
         typedef void type;
-        static type apply(LuaWrapper &l) {}
+        static type apply(LuaWrapper /*&instance*/) {}
     };
 
-    // Specialization for singular type
     template <typename T>
     struct _pop<1, T>
     {
         typedef T type;
-        static type apply(LuaWrapper &l)
+        static T apply(LuaWrapper &instance)
         {
             // fetch the top element (negative indices count from the top)
-            T ret  = l.fetch<T>(-1);
+            T ret  = instance.fetch<T>(-1);
 
-            // Remove it from the stack
-            LuaWrapper::popStack(l.m_state, 1);
+            LuaWrapper::popStack(instance.m_state, 1);
             return ret;
         }
     };
 
-
     template <typename... T>
     typename _pop<sizeof...(T), T...>::type pop()
     {
-        // Store the elements requested
         return _pop<sizeof...(T), T...>::apply(*this);
     }
+
 
 public:
     template <typename... Ret, typename... Args>
     typename _pop<sizeof...(Ret), Ret...>::type call(const std::string &fun, const Args&... args)
     {
-        // Push function to stack
         pushFunc(fun.c_str());
+        push(args...);
 
         const int numArgs = sizeof...(Args);
         const int numRet = sizeof...(Ret);
 
-        // Push all arguments to the stack using our variadic Push
-        push(args...);
-
-        // Call the function
         callFunc(numArgs, numRet);
 
-        // Return all the results and remove the from the stack
         return pop<Ret...>();
     }
 
@@ -153,6 +118,7 @@ protected:
 
     std::vector<std::string> m_scripts;
     int m_err;
+
 
 public:
     void operator=(LuaWrapper &) = delete;
