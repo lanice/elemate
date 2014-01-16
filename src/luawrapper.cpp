aa6f8e7beb330e706ad49b7dabe42fd2cf390301
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2014-01-16 13:30:04 +0100

    complete rework of function-calling. #76
    * no more need to call callFunc() and fetchResults() seperately.
    * function calling and return values receiving now with LuaWrapper::call<RetTypes...>(funcName, Args...)
    * single return value just stored in variable reference, multiple return values stored in std::tuple unpacked with std::tie

--- src/luawrapper.cpp
+++ src/luawrapper.cpp
@@ -57,10 +57,14 @@ void LuaWrapper::pushFunc(const std::string & func)
     lua_getglobal(m_state, func.c_str());
 }
 
-void LuaWrapper::callFunction(const int & argCount, const int & resultCount)
+void LuaWrapper::callFunc(const int & numArgs, const int & numRet)
 {
-    m_err = lua_pcall(m_state, argCount, resultCount, 0);
+    m_err = lua_pcall(m_state, numArgs, numRet, 0);
     luaError();
+}
+
+void LuaWrapper::push()
+{
 }
 
 void LuaWrapper::push(const std::string & value)
@@ -147,4 +151,35 @@ void LuaWrapper::fetchResult(bool & result)
     }
     result = lua_toboolean(m_state, -1) != 0;
     lua_pop(m_state, 1);
+}
+
+
+template<>
+std::string LuaWrapper::fetch<std::string>(const int index) const
+{
+    return lua_tostring(m_state, index);
+}
+
+template<>
+int LuaWrapper::fetch<int>(const int index) const
+{
+    return lua_tointeger(m_state, index);
+}
+
+template<>
+double LuaWrapper::fetch<double>(const int index) const
+{
+    return lua_tonumber(m_state, index);
+}
+
+template<>
+unsigned long LuaWrapper::fetch<unsigned long>(const int index) const
+{
+    return lua_tounsigned(m_state, index);
+}
+
+template<>
+bool LuaWrapper::fetch<bool>(const int index) const
+{
+    return lua_toboolean(m_state, index);
 }
