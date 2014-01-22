e0530d4b8b80a0f2e363790fd3573ad3a303d7c4
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2014-01-22 14:21:16 +0100

    add basic ParticleGroup interface. #85

--- src/particlegroup.h
+++ src/particlegroup.h
@@ -1,9 +1,54 @@
 #pragma once
 
 
+struct ImmutableParticleProperties
+{
+    physx::PxReal maxMotionDistance;
+    physx::PxReal gridSize;
+    physx::PxReal restOffset;
+    physx::PxReal contactOffset;
+
+    physx::PxReal restParticleDistance;
+};
+
+struct MutableParticleProperties
+{
+    physx::PxReal restitution;
+    physx::PxReal dynamicFriction;
+    physx::PxReal staticFriction;
+    physx::PxReal damping;
+    physx::PxVec3 externalAcceleration;
+    physx::PxReal particleMass;
+
+    physx::PxReal viscosity;
+    physx::PxReal stiffness;
+};
+
 class ParticleGroup
 {
 public:
-    ParticleGroup();
+    ParticleGroup(
+        const physx::PxU32 maxParticleCount,
+        const ImmutableParticleProperties & immutProperties,
+        const MutableParticleProperties & mutProperties
+        );
     ~ParticleGroup();
+
+    void createParticles(const physx::PxU32 number, const physx::PxVec3 & position);
+    void startEmit(const physx::PxU32 particleRate, const physx::PxVec3 & position);
+    void stopEmit();
+
+    void update();
+
+    void setImmutableProperties(const ImmutableParticleProperties & properties);
+    void setMutableProperties(const MutableParticleProperties & properties);
+
+
+private:
+    ImmutableParticleProperties m_immutProperties;
+    MutableParticleProperties m_mutProperties;
+
+public:
+    ParticleGroup(ParticleGroup&) = delete;
+    void operator=(ParticleGroup&) = delete;
 };
