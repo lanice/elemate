9fd53c9cd0a9dd4b5b5009b95823ff6252634936
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2014-02-05 08:09:17 +0100

    fix start.lua. #105

--- scripts/start.lua
+++ scripts/start.lua
@@ -1,6 +1,6 @@
 -- Lua script for tweaking terrain / setting emitters at startup (start.lua)
 
 
-local id = psa_createParticleGroup("water")
+local id = psa_createParticleGroup("water", 10000)
 
 psa_emit(id, 500, 105.446, 7.953, 95.338, 1, 0, 0)
