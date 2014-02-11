1c0cc3f16cdd496f87e01129d8c6339059117fb7
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2014-02-11 23:24:40 +0100

    move elemate code to separate library, add sub projects for the scripts and shader #114

--- cmake/Custom.cmake
+++ cmake/Custom.cmake
@@ -3,11 +3,34 @@
 # Example: source_group_by_path("${CMAKE_CURRENT_SOURCE_DIR}/src" 
 #   "\\\\.h$|\\\\.hpp$|\\\\.cpp$|\\\\.c$|\\\\.ui$|\\\\.qrc$" "Source Files" ${sources})
 
-macro(source_group_by_path PARENT_PATH REGEX GROUP)
+macro(source_subgroup_by_path PARENT_PATH REGEX GROUP)
 
     set(args ${ARGV})
 
     list(REMOVE_AT args 0)
+    list(REMOVE_AT args 0)
+    list(REMOVE_AT args 0)
+
+    foreach(FILENAME ${args})
+
+        get_filename_component(FILEPATH "${FILENAME}" REALPATH)
+        file(RELATIVE_PATH FILEPATH ${PARENT_PATH} ${FILEPATH})
+        get_filename_component(FILEPATH "${FILEPATH}" PATH)
+
+        string(REPLACE "/" "\\" FILEPATH "${FILEPATH}")
+
+        if(${FILENAME} MATCHES "${REGEX}")
+            source_group("${GROUP}\\${FILEPATH}" FILES ${FILENAME})  
+        endif()
+
+    endforeach()
+
+endmacro()
+
+macro(source_group_by_path PARENT_PATH REGEX)
+
+    set(args ${ARGV})
+
     list(REMOVE_AT args 0)
     list(REMOVE_AT args 0)
 
