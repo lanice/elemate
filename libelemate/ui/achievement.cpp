a56ef66b7b5b63ced5a6cba20a67885c79911ce2
FHorschig <f.horschig@googlemail.com>
2014-02-20 11:00:35 +0100

    Changed tabs to spaces. #110

--- libelemate/ui/achievement.cpp
+++ libelemate/ui/achievement.cpp
@@ -20,164 +20,164 @@ m_title(title)
 , m_unlocked(unlocked)
 , m_picture(picture)
 {
-	initialize();
+    initialize();
 }
 
 void Achievement::initialize()
 {
-	const int TEXTURE_SIZE = 256;
-	
-	m_stringDrawer.initialize();
-	std::vector<glm::vec2> points({
-		glm::vec2(+1.f, -1.f)
-		, glm::vec2(+1.f, +1.f)
-		, glm::vec2(-1.f, -1.f)
-		, glm::vec2(-1.f, +1.f)
-	});
+    const int TEXTURE_SIZE = 256;
+    
+    m_stringDrawer.initialize();
+    std::vector<glm::vec2> points({
+        glm::vec2(+1.f, -1.f)
+        , glm::vec2(+1.f, +1.f)
+        , glm::vec2(-1.f, -1.f)
+        , glm::vec2(-1.f, +1.f)
+    });
 
-	glow::Buffer* vbo = new glow::Buffer(GL_ARRAY_BUFFER);
-	vbo->setData(points, GL_STATIC_DRAW);
+    glow::Buffer* vbo = new glow::Buffer(GL_ARRAY_BUFFER);
+    vbo->setData(points, GL_STATIC_DRAW);
 
-	m_vao = new glow::VertexArrayObject();
+    m_vao = new glow::VertexArrayObject();
 
-	m_vao->bind();
+    m_vao->bind();
 
-	glow::VertexAttributeBinding * vertexBinding = m_vao->binding(0);
+    glow::VertexAttributeBinding * vertexBinding = m_vao->binding(0);
 
-	vertexBinding->setAttribute(0);
-	vertexBinding->setBuffer(vbo, 0, sizeof(glm::vec2));
-	vertexBinding->setFormat(2, GL_FLOAT, GL_FALSE, 0);
-	m_vao->enable(0);
+    vertexBinding->setAttribute(0);
+    vertexBinding->setBuffer(vbo, 0, sizeof(glm::vec2));
+    vertexBinding->setFormat(2, GL_FLOAT, GL_FALSE, 0);
+    m_vao->enable(0);
 
-	m_vao->unbind();
+    m_vao->unbind();
 
-	m_program = new glow::Program();
-	m_program->attach(
-		glowutils::createShaderFromFile(GL_VERTEX_SHADER, "shader/ui/achievement.vert"),
-		glowutils::createShaderFromFile(GL_FRAGMENT_SHADER, "shader/ui/achievement.frag"));
+    m_program = new glow::Program();
+    m_program->attach(
+        glowutils::createShaderFromFile(GL_VERTEX_SHADER, "shader/ui/achievement.vert"),
+        glowutils::createShaderFromFile(GL_FRAGMENT_SHADER, "shader/ui/achievement.frag"));
 
-	m_program->setUniform("picture", 0);
+    m_program->setUniform("picture", 0);
 
-	m_texture = new glow::Texture(GL_TEXTURE_2D);
-	m_texture->setParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	m_texture->setParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	m_texture->setParameter(GL_TEXTURE_WRAP_S, GL_REPEAT);
-	m_texture->setParameter(GL_TEXTURE_WRAP_T, GL_REPEAT);
+    m_texture = new glow::Texture(GL_TEXTURE_2D);
+    m_texture->setParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    m_texture->setParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    m_texture->setParameter(GL_TEXTURE_WRAP_S, GL_REPEAT);
+    m_texture->setParameter(GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	RawImage image("data/textures/achievements/" + m_picture + ".raw", TEXTURE_SIZE, TEXTURE_SIZE);
+    RawImage image("data/textures/achievements/" + m_picture + ".raw", TEXTURE_SIZE, TEXTURE_SIZE);
 
-	m_texture->bind();
-	m_texture->image2D(0, GL_RGB8, TEXTURE_SIZE, TEXTURE_SIZE, 0, GL_RGB, GL_UNSIGNED_BYTE, image.rawData());
-	m_texture->unbind();
+    m_texture->bind();
+    m_texture->image2D(0, GL_RGB8, TEXTURE_SIZE, TEXTURE_SIZE, 0, GL_RGB, GL_UNSIGNED_BYTE, image.rawData());
+    m_texture->unbind();
 }
 
 void Achievement::draw()
 {
-	update();
-	m_texture->bindActive(GL_TEXTURE0);
+    update();
+    m_texture->bindActive(GL_TEXTURE0);
 
-	m_program->setUniform("time_mod", m_timeMod);
-	m_program->setUniform("ratio", m_viewport.x / m_viewport.y);
+    m_program->setUniform("time_mod", m_timeMod);
+    m_program->setUniform("ratio", m_viewport.x / m_viewport.y);
 
-	m_program->use();
-	m_vao->bind();
+    m_program->use();
+    m_vao->bind();
 
-	m_vao->drawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    m_vao->drawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-	m_vao->unbind();
-	m_program->release();
+    m_vao->unbind();
+    m_program->release();
 
-	m_texture->unbindActive(GL_TEXTURE0);
-	float pos = 0.9f + m_timeMod;
-	m_stringDrawer.paint(m_title,
-		glm::mat4(0.5, 0, 0, 0,
-		0, 0.5, 0, 0,
-		0, 0, 0.5, 0,
-		0.7, pos, 0, 1));
-	pos -= 0.06f;
+    m_texture->unbindActive(GL_TEXTURE0);
+    float pos = 0.9f + m_timeMod;
+    m_stringDrawer.paint(m_title,
+        glm::mat4(0.5, 0, 0, 0,
+        0, 0.5, 0, 0,
+        0, 0, 0.5, 0,
+        0.7, pos, 0, 1));
+    pos -= 0.06f;
 
-	for (auto& line : splitText(m_text,25))
-	{
-		m_stringDrawer.paint(line,
-			glm::mat4(0.25, 0, 0, 0,
-			0, 0.25, 0, 0,
-			0, 0, 0.25, 0,
-			0.7, pos, 0, 1));
-		pos -= 0.05f;
-	}
+    for (auto& line : splitText(m_text,25))
+    {
+        m_stringDrawer.paint(line,
+            glm::mat4(0.25, 0, 0, 0,
+            0, 0.25, 0, 0,
+            0, 0, 0.25, 0,
+            0.7, pos, 0, 1));
+        pos -= 0.05f;
+    }
 }
 
 std::list<std::string> Achievement::splitText(std::string text, size_t maxLength)
 {
-	std::list<std::string> split;
-	size_t pos = 0;
-	size_t last_split_pos = 0;
-	while (!text.empty()){
-		while (pos < maxLength && pos <= text.length()){
-			if (text[pos] == ' ' || text[pos] == '\n' || text[pos] == '-' || text[pos] == '\0')
-				last_split_pos = pos;
-			if (text[pos] == '\n')
-				break;
-			pos++;
-		}
-		if (!last_split_pos)
-			last_split_pos = maxLength;
-		if (text.length() > last_split_pos)	{
-			std::string nextLine(text.substr(0, last_split_pos));
-			if (' ' == nextLine[0] || '\n' == nextLine[0])
-				nextLine = nextLine.substr(1);
-			split.push_back(nextLine);
-			text = text.substr(last_split_pos);
-			pos = 0;
-		}
-		else {
-			split.push_back(text);
-			text = "";
-		}
-	}
-	return split;
+    std::list<std::string> split;
+    size_t pos = 0;
+    size_t last_split_pos = 0;
+    while (!text.empty()){
+        while (pos < maxLength && pos <= text.length()){
+            if (text[pos] == ' ' || text[pos] == '\n' || text[pos] == '-' || text[pos] == '\0')
+                last_split_pos = pos;
+            if (text[pos] == '\n')
+                break;
+            pos++;
+        }
+        if (!last_split_pos)
+            last_split_pos = maxLength;
+        if (text.length() > last_split_pos) {
+            std::string nextLine(text.substr(0, last_split_pos));
+            if (' ' == nextLine[0] || '\n' == nextLine[0])
+                nextLine = nextLine.substr(1);
+            split.push_back(nextLine);
+            text = text.substr(last_split_pos);
+            pos = 0;
+        }
+        else {
+            split.push_back(text);
+            text = "";
+        }
+    }
+    return split;
 }
 
 void Achievement::update(){
-	std::chrono::duration<double> diff = std::chrono::system_clock::now() - m_unlockTime;
-	m_timeMod = static_cast<float>(1.0 - diff.count()/ACHIEVEMENT_DISPLAY_TIME);
-	m_timeMod *= m_timeMod;
-	for (int i = 0; i < ACHIEVEMENT_DISPLAY_TIME;i++)
-		m_timeMod *= m_timeMod*m_timeMod;
-	if (diff.count() > 2*ACHIEVEMENT_DISPLAY_TIME)
-	{
-		unlock();//To reset the time. But later ti hide the achievement
-	}
+    std::chrono::duration<double> diff = std::chrono::system_clock::now() - m_unlockTime;
+    m_timeMod = static_cast<float>(1.0 - diff.count()/ACHIEVEMENT_DISPLAY_TIME);
+    m_timeMod *= m_timeMod;
+    for (int i = 0; i < ACHIEVEMENT_DISPLAY_TIME;i++)
+        m_timeMod *= m_timeMod*m_timeMod;
+    if (diff.count() > 2*ACHIEVEMENT_DISPLAY_TIME)
+    {
+        unlock();//To reset the time. But later ti hide the achievement
+    }
 }
 
 void Achievement::lock()
 {
-	m_unlocked = false;
+    m_unlocked = false;
 }
 
 void Achievement::unlock()
 {
-	m_unlocked = true;
-	m_unlockTime = std::chrono::system_clock::now();
+    m_unlocked = true;
+    m_unlockTime = std::chrono::system_clock::now();
 }
 
 bool Achievement::isUnlocked() const
 {
-	return m_unlocked;
+    return m_unlocked;
 }
 
 std::string Achievement::title() const
 {
-	return m_title;
+    return m_title;
 }
 
 std::string Achievement::text() const
 {
-	return m_text;
+    return m_text;
 }
 
 void Achievement::resize(int width, int height)
 {
-	m_viewport = glm::vec2(width, height);
-	m_stringDrawer.resize(width, height);
+    m_viewport = glm::vec2(width, height);
+    m_stringDrawer.resize(width, height);
 }
--- libelemate/ui/achievement.h
+++ libelemate/ui/achievement.h
@@ -9,8 +9,8 @@
 #include <chrono>
 
 namespace glow{
-	class VertexArrayObject;
-	class Program;
+    class VertexArrayObject;
+    class Program;
 }
 
 #include <string>
@@ -18,40 +18,40 @@ namespace glow{
 class Achievement
 {
 public:
-	Achievement(const std::string& title, const std::string& text = "", bool unlocked = false, const std::string& picture = "default");
+    Achievement(const std::string& title, const std::string& text = "", bool unlocked = false, const std::string& picture = "default");
 
-	void lock();
-	void unlock();
-	bool isUnlocked() const;
+    void lock();
+    void unlock();
+    bool isUnlocked() const;
 
-	void initialize();
-	void update();
-	void draw();
-	void resize(int width, int height);
+    void initialize();
+    void update();
+    void draw();
+    void resize(int width, int height);
 
-	std::string title() const;
-	std::string text() const;
+    std::string title() const;
+    std::string text() const;
 
-	static std::list<std::string> splitText(std::string text, size_t maxLength);
+    static std::list<std::string> splitText(std::string text, size_t maxLength);
 protected:
-	static const float ACHIEVEMENT_DISPLAY_TIME;
+    static const float ACHIEVEMENT_DISPLAY_TIME;
 
-	float		m_timeMod;
-	std::chrono::time_point<std::chrono::system_clock>	m_unlockTime;
+    float       m_timeMod;
+    std::chrono::time_point<std::chrono::system_clock>  m_unlockTime;
 
-	std::string m_title;
-	std::string m_text;
-	bool		m_unlocked;
-	std::string m_picture;
+    std::string m_title;
+    std::string m_text;
+    bool        m_unlocked;
+    std::string m_picture;
 
-	StringDrawer m_stringDrawer;
+    StringDrawer m_stringDrawer;
 
-	glow::ref_ptr<glow::VertexArrayObject>	m_vao;
-	glow::ref_ptr<glow::Program>			m_program;
-	glm::vec2								m_viewport;
-	glow::ref_ptr<glow::Texture>			m_texture;
+    glow::ref_ptr<glow::VertexArrayObject>  m_vao;
+    glow::ref_ptr<glow::Program>            m_program;
+    glm::vec2                               m_viewport;
+    glow::ref_ptr<glow::Texture>            m_texture;
 
 private:
-	Achievement(const Achievement&) = delete;
-	void operator=(const Achievement&) = delete;
+    Achievement(const Achievement&) = delete;
+    void operator=(const Achievement&) = delete;
 };
